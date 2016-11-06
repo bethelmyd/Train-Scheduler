dab1083e4a4e533d225ad053fb1c6b3b11a4e3bd
bethelmyd <bethelmyd@gmail.com>
2016-11-06 02:46:08 -0500

    problem with time format

--- assets/js/scheduler.js
+++ assets/js/scheduler.js
@@ -1,3 +1,5 @@
+"use strict";
+
       // Initialize Firebase
       var config = {
         apiKey: "AIzaSyBuKCWponF0dCeDh2w3al3BTtTdvYpSjj0",
@@ -12,55 +14,90 @@
 
 
     // Initial Values
-    var name = "";
-    var email = "";
-    var age = 0;
-    var comment = "";
+    var trainName = "";
+    var destination = "";
+    var firstTime = 0;
+    var frequency = 0;
 
 
     // Capture Button Click
-    $("#addUser").on("click", function () {
+    $("#submitBtn").on("click", function (e) {
+        e.preventDefault();
 
         // YOUR TASK!!!
-        // Code in the logic for storing and retrieving the most recent user.
+        // Code in the logic for storing and retrieving the most recent train info.
         // Dont forget to provide initial data to your Firebase database.
-        name = $('#nameinput').val().trim();
-        email = $('#emailinput').val().trim();
-        age = $('#ageinput').val().trim();
-        comment = $('#commentinput').val().trim();
+        var trainName = $('#trainName').val().trim();
+        var destination = $('#destination').val().trim();
+        var firstTime = $('#firstTime').val().trim();
+        //firstTime = moment(firstTime).format("HH:mm");
+        // if(firstTime > moment())
+        // {
+        //     return;
+        // }
+        var frequency = parseInt($('#frequency').val().trim());
 
 
         // Code for the push
         dataRef.ref().push({
-            name: name,
-            email: email,
-            age: age,
-            comment: comment,
+            trainName: trainName,
+            destination: destination,
+            firstTime: firstTime,
+            frequency: frequency,
             dateAdded: firebase.database.ServerValue.TIMESTAMP
-        })
+        });
         // Don't refresh the page!
         return false;
     });
 
     //Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
     dataRef.ref().on("child_added", function (childSnapshot) {
+
+        if(childSnapshot == null) return;
+
         // Log everything that's coming out of snapshot
-        console.log(childSnapshot.val().name);
-        console.log(childSnapshot.val().email);
-        console.log(childSnapshot.val().age);
-        console.log(childSnapshot.val().comment);
+        console.log(childSnapshot.val().trainName);
+        console.log(childSnapshot.val().destination);
+        console.log(moment(childSnapshot.val().firstTime).minutes());
+        console.log(childSnapshot.val().frequency);
         console.log(childSnapshot.val().dateAdded);
 
-        // full list of items to the well
+        //calculate next arrival time and minutes away
+        // First Time (pushed back 1 year to make sure it comes before current time)
+        var firstTimeConverted = moment(childSnapshot.val().firstTime,"HH:mm"); //.subtract(1, "years");
+        console.log(firstTimeConverted);
 
-        var html = "<div class='well'>";
-        html += "<span class='name'>Name: " + childSnapshot.val().name + "</span> ";
-        html += "<span class='email'>Email: " + childSnapshot.val().email + "</span> ";
-        html += "<span class='age'>Age: " + childSnapshot.val().age + "</span> ";
-        html += "<span class='comment'>Comment: " + childSnapshot.val().comment + "</span>";
-        html += "</div>";
+        // Current Time
+        var currentTime = moment();
+        console.log("CURRENT TIME: " + moment(currentTime).format("HH:mm"));
 
-        $('#full-member-list').append(html);
+        // Difference between the times
+        var diffTime = currentTime.diff(moment(firstTimeConverted), "minutes");
+        console.log("DIFFERENCE IN TIME: " + diffTime);
+
+        // Time apart (remainder)
+        var tRemainder = diffTime % childSnapshot.val().frequency;
+        console.log(tRemainder);
+
+        // Minute Until Train
+        var tMinutesTillTrain = childSnapshot.val().frequency - tRemainder;
+        console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+        // Next Train
+        var nextTrain = moment().add(tMinutesTillTrain, "minutes")
+        console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm A"))
+
+        // make row
+
+        var row = "<tr>";
+        row += "<td>" + childSnapshot.val().trainName + "</td> ";
+        row += "<td>" + childSnapshot.val().destination + "</td> ";
+        row += "<td>" + childSnapshot.val().frequency + "</td> ";
+        row += "<td>" + moment(nextTrain).format("hh:mm A") + "</td>";
+        row += "<td>" + tMinutesTillTrain + "</td>";
+        row += "</tr>";
+
+        $('#rowSpace').append(row);
 
 
 // Handle the errors
@@ -68,12 +105,12 @@
         //console.log("Errors handled: " + errorObject.code)
     });
 
-    dataRef.ref().orderByChild("dateAdded").limitToLast(1)
-            .on("child_added", function (snapshot) {
-        // Change the HTML to reflect
-        $("#namedisplay").html(snapshot.val().name);
-        $("#emaildisplay").html(snapshot.val().email);
-        $("#agedisplay").html(snapshot.val().age);
-        $("#commentdisplay").html(snapshot.val().comment);
-    });
+    // dataRef.ref().orderByChild("dateAdded").limitToLast(1)
+    //         .on("child_added", function (snapshot) {
+    //     // Change the HTML to reflect
+    //     $("#namedisplay").html(snapshot.val().name);
+    //     $("#emaildisplay").html(snapshot.val().email);
+    //     $("#agedisplay").html(snapshot.val().age);
+    //     $("#commentdisplay").html(snapshot.val().comment);
+    // });
 
